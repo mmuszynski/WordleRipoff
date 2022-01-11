95258110ec41f1122729683022ed4511da2618d9
Mike Muszynski <mmuszynski@gmail.com>
2022-01-11 16:48:03 -0500

    Initial Commit

---
+++ Shared/WordleRipoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleRipoffApp.swift
+//  Shared
+//
+//  Created by Mike Muszynski on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleRipoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
